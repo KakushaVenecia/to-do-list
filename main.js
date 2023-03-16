3365543e7387fd9fd0ef96fc9b788695d2e4a89f
KakushaVenecia <kakushavenecia@gmail.com>
2023-03-16 19:26:45 +0300

    This is the initial commit of the project boiler plate
    Set up of the pages, font awesome and script file

---
+++ main.js
@@ -0,0 +1,8 @@
+function getStarted() {
+    const getStarted = document.getElementById("theForm");
+    if (getStarted.style.display === "none") {
+      getStarted.style.display = "block";
+    } else {
+      getStarted.style.display = "none";
+    }
+  }
